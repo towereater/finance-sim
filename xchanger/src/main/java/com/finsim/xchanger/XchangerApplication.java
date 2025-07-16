77ac5883a99bb195eafa4a2ff9c474b54d3afab3
Andrea <andrea.n96@hotmail.it>
2025-07-16 13:56:12 +0200

    xchanger first commit

---
+++ xchanger/src/main/java/com/finsim/xchanger/XchangerApplication.java
@@ -0,0 +1,13 @@
+package com.finsim.xchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XchangerApplication.class, args);
+	}
+
+}
---
+++ xchanger/src/test/java/com/finsim/xchanger/XchangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsim.xchanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XchangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
