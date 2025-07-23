7bf4793e844368cb5ff6109ca09c520dfad655a0
Andrea <andrea.n96@hotmail.it>
2025-07-23 14:00:26 +0200

    Removed unused function

--- xchanger/src/main/java/com/finsim/xchanger/db/DossierRepository.java
+++ xchanger/src/main/java/com/finsim/xchanger/db/DossierRepository.java
@@ -1,11 +1,9 @@
 package com.finsim.xchanger.db;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.finsim.xchanger.entity.Dossier;
 
 public interface DossierRepository extends MongoRepository<Dossier, String> {
-  public List<Dossier> findByOwner(String owner);
+  
 }
