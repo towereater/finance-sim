27e1020155e6845c84dfaa1fd61654887e2ae89e
Andrea <andrea.n96@hotmail.it>
2025-08-31 15:37:54 +0200

    Changed xchanger dependencies

--- xchanger/build.gradle.kts
+++ xchanger/build.gradle.kts
@@ -26,17 +26,14 @@ repositories {
 dependencies {
 	//implementation("org.springframework.boot:spring-boot-starter-actuator")
 	//implementation("org.springframework.boot:spring-boot-starter-batch")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	//implementation(platform("org.mongodb:mongodb-driver-bom:5.5.1"))
 	implementation("org.mongodb:mongodb-driver-sync")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	//testImplementation("org.springframework.boot:spring-boot-starter-test")
 	//testImplementation("org.springframework.batch:spring-batch-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
+	//testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
