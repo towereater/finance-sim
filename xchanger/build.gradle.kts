55fd01b499d6a6fd9d78c1bff8ec99c23433ccbe
Andrea <andrea.n96@hotmail.it>
2025-09-19 23:02:58 +0200

    Updated spring version

--- xchanger/build.gradle.kts
+++ xchanger/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.5"
+	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -31,8 +31,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	//implementation(platform("org.mongodb:mongodb-driver-bom:5.5.1"))
 	implementation("org.mongodb:mongodb-driver-sync")
-	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+	compileOnly("org.projectlombok:lombok")
 	//testImplementation("org.springframework.boot:spring-boot-starter-test")
 	//testImplementation("org.springframework.batch:spring-batch-test")
 	//testRuntimeOnly("org.junit.platform:junit-platform-launcher")
