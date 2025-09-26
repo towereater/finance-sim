9df3ab4a50454046364ed493066d269bc137de8c
Andrea <andrea.n96@hotmail.it>
2025-09-27 01:51:36 +0200

    Payment request create function

--- xchanger/src/main/java/com/finsim/xchanger/payments/service/PaymentService.java
+++ xchanger/src/main/java/com/finsim/xchanger/payments/service/PaymentService.java
@@ -9,8 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.finsim.xchanger.payments.model.AccountIdentification;
 import com.finsim.xchanger.payments.model.InsertPaymentRequest;
+import com.finsim.xchanger.payments.model.Payee;
+import com.finsim.xchanger.payments.model.Payer;
 import com.finsim.xchanger.payments.model.Payment;
+import com.finsim.xchanger.payments.model.PaymentValue;
 
 @Service
 public class PaymentService {
@@ -19,6 +23,17 @@ public class PaymentService {
 
     @Value("${xchanger.payments.host}")
     private String baseUrl;
+
+    public InsertPaymentRequest createBankTransfer(PaymentValue value, String payerIban, String payeeName, String payeeIban, String details) {
+        InsertPaymentRequest req = new InsertPaymentRequest();
+        req.setType("BANK_TRANSFER");
+        req.setValue(value);
+        req.setPayer(new Payer(new AccountIdentification("IBAN", payerIban)));
+        req.setPayee(new Payee(payeeName, new AccountIdentification("IBAN", payeeIban)));
+        req.setDetails(details);
+
+        return req;
+    }
 
     public ResponseEntity<Payment> insertPayment(String auth, InsertPaymentRequest insertPaymentRequest) {
         HttpHeaders headers = new HttpHeaders();
