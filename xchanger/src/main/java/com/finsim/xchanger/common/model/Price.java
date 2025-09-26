ed07be3bafe14a081e855be5fde210c2a5f8a8f8
Andrea <andrea.n96@hotmail.it>
2025-09-27 01:50:56 +0200

    Added some useful operators

--- xchanger/src/main/java/com/finsim/xchanger/common/model/Price.java
+++ xchanger/src/main/java/com/finsim/xchanger/common/model/Price.java
@@ -1,5 +1,7 @@
 package com.finsim.xchanger.common.model;
 
+import com.finsim.xchanger.payments.model.PaymentValue;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +12,17 @@ import lombok.NoArgsConstructor;
 public class Price {
     public float amount;
     public String currency;
+
+    public Price multiplyBy(float multiplier) {
+        return new Price(this.amount * multiplier, this.currency);
+    }
+
+    public PaymentValue toPaymentValue() {
+        return new PaymentValue(this.amount, this.currency);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%.2f %s", this.amount, this.currency);
+    }
 }
