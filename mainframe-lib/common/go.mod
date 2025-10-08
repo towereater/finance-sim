6404013e78a49a54cbe586d73061b7ec14dc0772
Andrea <andrea.n96@hotmail.it>
2025-10-08 10:48:33 +0200

    Added kafka common services

--- mainframe-lib/common/go.mod
+++ mainframe-lib/common/go.mod
@@ -4,6 +4,7 @@ go 1.24.5
 
 require (
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/segmentio/kafka-go v0.4.49
 	go.mongodb.org/mongo-driver v1.17.4
 )
 
@@ -11,6 +12,7 @@ require (
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
---
+++ mainframe-lib/common/queue/queue.go
@@ -0,0 +1,50 @@
+package queue
+
+import (
+	"context"
+	"mainframe-lib/common/config"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func QueueContent(ctx context.Context, queue config.Queue, topic string, payload string) error {
+	// Create topic writer
+	w := &kafka.Writer{
+		Addr:                   kafka.TCP(queue.Brokers...),
+		Topic:                  topic,
+		AllowAutoTopicCreation: true,
+	}
+
+	// Prepare data for queue
+	key := []byte(time.Now().Format(time.DateTime))
+	value := []byte(payload)
+
+	// Write data on queue
+	err := w.WriteMessages(ctx,
+		kafka.Message{
+			Key:   key,
+			Value: value,
+		},
+	)
+
+	return err
+}
+
+func UnqueueContent(ctx context.Context, queue config.Queue, topic string) (string, error) {
+	// Create topic reader
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: queue.Brokers,
+		GroupID: queue.Group,
+		Topic:   topic,
+	})
+	defer r.Close()
+
+	// Read the message from queue
+	m, err := r.ReadMessage(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	return string(m.Value), nil
+}
