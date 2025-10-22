a1f0823d1c0fb29e0215aba5d140740da20823e3
Andrea <andrea.n96@hotmail.it>
2025-10-22 10:49:35 +0200

    Small refactoring to logs

--- processor/payment/main.go
+++ processor/payment/main.go
@@ -38,14 +38,14 @@ func main() {
 		// Poll the queue for data
 		key, paymentId, err := service.UnqueuePayment(cfg.Queue)
 		if err != nil {
-			fmt.Printf("Error while reading queue: %s", err.Error())
+			fmt.Printf("Error while reading queue: %s\n", err.Error())
 			continue
 		}
 
 		// Get abi value
 		abi := strings.Split(key, ":")[0]
 		if abi == "" || len(abi) != 5 {
-			fmt.Printf("Queue content with key %s has invalid abi value", key)
+			fmt.Printf("Queue content with key %s has invalid abi value\n", key)
 			continue
 		}
 
@@ -54,18 +54,18 @@ func main() {
 		// Get payment data
 		payment, err := db.SelectPayment(cfg.DB, abi, paymentId)
 		if err == mongo.ErrNoDocuments {
-			fmt.Printf("No payment with id %s found", paymentId)
+			fmt.Printf("No payment with id %s found\n", paymentId)
 			continue
 		}
 		if err != nil {
-			fmt.Printf("Error while searching payment %s: %s", paymentId, err.Error())
+			fmt.Printf("Error while searching payment %s: %s\n", paymentId, err.Error())
 			continue
 		}
 
 		// Process payment data
 		outcome, err := service.ProcessPayment(cfg, abi, payment)
 		if err != nil {
-			fmt.Printf("Error while processing payment %s: %s", paymentId, err.Error())
+			fmt.Printf("Error while processing payment %s: %s\n", paymentId, err.Error())
 			continue
 		}
 
@@ -73,7 +73,7 @@ func main() {
 		payment.Outcome = outcome
 		err = db.UpdatePayment(cfg.DB, abi, payment)
 		if err != nil {
-			fmt.Printf("Error while updating payment %s: %s", paymentId, err.Error())
+			fmt.Printf("Error while updating payment %s: %s\n", paymentId, err.Error())
 			continue
 		}
 
