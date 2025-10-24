fb77ca7f51685805e222a605a0924034b60b149c
Andrea <andrea.n96@hotmail.it>
2025-10-25 01:03:08 +0200

    Added dossier update function

--- xchanger/src/main/java/com/finsim/xchanger/dossiers/service/DossierService.java
+++ xchanger/src/main/java/com/finsim/xchanger/dossiers/service/DossierService.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.finsim.xchanger.common.model.Price;
 import com.finsim.xchanger.dossiers.model.Dossier;
+import com.finsim.xchanger.dossiers.model.DossierValue;
 import com.finsim.xchanger.dossiers.model.InsertDossierRequest;
 import com.finsim.xchanger.dossiers.repository.DossierRepository;
 
+import java.time.Instant;
 import java.util.Optional;
 
 @Service
@@ -32,12 +35,17 @@ public class DossierService {
         dossier.setAbi(abi);
         dossier.setExternalId(dossierRequest.externalId);
         dossier.setIban(dossierRequest.iban);
+        dossier.setValue(new DossierValue(new Price(0, "EUR"), Instant.now().toString()));
 
         return insertDossier(dossier);
     }
 
     public Dossier insertDossier(Dossier dossier) {
         return dossierRepository.insert(dossier);
+    }
+
+    public Dossier updateDossier(Dossier dossier) {
+        return dossierRepository.save(dossier);
     }
 
     public void deleteDossier(String id) {
