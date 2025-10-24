861acd788b782f453b7ac302cb810a3163ec226e
Andrea <andrea.n96@hotmail.it>
2025-10-25 01:03:55 +0200

    Added new functions

--- xchanger/src/main/java/com/finsim/xchanger/orders/repository/OrderRepository.java
+++ xchanger/src/main/java/com/finsim/xchanger/orders/repository/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.finsim.xchanger.orders.repository;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -9,8 +9,14 @@ import com.finsim.xchanger.orders.model.Order;
 
 public interface OrderRepository extends MongoRepository<Order, String> {
     @Query(value="{ 'isin': ?0 }", hint="order-buy-index")
-    List<Order> findBestBuyOrders(String isin);
+    Page<Order> findBestBuyOrders(String isin, Pageable pageable);
+
+    @Query(value="{ 'isin': ?0, 'price.amount': { '$gte': ?1 } }", hint="order-buy-index")
+    Page<Order> findBestBuyOrdersWithPriceGreaterThan(String isin, float amount, Pageable pageable);
 
     @Query(value="{ 'isin': ?0 }", hint="order-sell-index")
-    List<Order> findBestSellOrders(String isin);
+    Page<Order> findBestSellOrders(String isin, Pageable pageable);
+
+    @Query(value="{ 'isin': ?0, 'price.amount': { '$lte': ?1 } }", hint="order-sell-index")
+    Page<Order> findBestSellOrdersWithPriceLowerThan(String isin, float amount, Pageable pageable);
 }
