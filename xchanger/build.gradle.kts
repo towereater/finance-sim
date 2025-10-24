84676a7979ee8b1a1849170f517f9e6828b067b9
Andrea <andrea.n96@hotmail.it>
2025-10-25 00:59:21 +0200

    Updated spring version

--- xchanger/build.gradle.kts
+++ xchanger/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.5.6"
+	id("org.springframework.boot") version "3.5.7"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -24,17 +24,11 @@ repositories {
 }
 
 dependencies {
-	//implementation("org.springframework.boot:spring-boot-starter-actuator")
-	//implementation("org.springframework.boot:spring-boot-starter-batch")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-	//implementation(platform("org.mongodb:mongodb-driver-bom:5.5.1"))
 	implementation("org.mongodb:mongodb-driver-sync")
 	implementation("org.springframework.kafka:spring-kafka")
 	annotationProcessor("org.projectlombok:lombok")
 	compileOnly("org.projectlombok:lombok")
-	//testImplementation("org.springframework.boot:spring-boot-starter-test")
-	//testImplementation("org.springframework.batch:spring-batch-test")
-	//testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
